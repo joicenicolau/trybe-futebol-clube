907938a6b5108f99db7b21eb1bbb3145439f64ab
Joice Nicolau <joice_nicolau@hotmail.com>
2023-06-14 16:35:49 -0300

    Req 10

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from 'express';
 import UserService from '../services/UserService';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
 
 export default class UserController {
   constructor(
     private userService: UserService,
   ) { }
 
+  // pesquisa: https://github.com/tryber/sd-027-a-live-lectures/blob/lecture/back/10.1/src/controllers/UserController.ts
   public async login(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
+
     const serviceResponse = await this.userService.login(email, password);
 
-    if (!serviceResponse) {
-      return res.status(400).json({ message: 'All fields must be filled' });
+    if (serviceResponse.status !== 'SUCCESSFUL') {
+      return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
     }
-    return res.status(200).json({ token: serviceResponse.token });
+    return res.status(200).json(serviceResponse.data);
   }
 }
--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,30 +1,30 @@
 import { TokenGenerate } from '../Interfaces/token/TokenGenerate';
 import { Encrypter } from '../Interfaces/token/Encrypter';
-// import { IUser } from '../Interfaces/user/IUser';
 import UserModel from '../database/models/UserModel';
+import { ServiceResponse } from '../Interfaces/ServiceResponse';
 
 export default class UserService {
   constructor(
     private userModel: typeof UserModel,
     private encrypter: Encrypter,
     private tokenGenerator: TokenGenerate,
-  ) { }
+  ) {}
 
-  public async login(email: string, password: string): Promise<{ token: string }> {
+  public async login(email: string, password: string): Promise<ServiceResponse<{ token: string }>> {
     const user = await this.userModel.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Invalid email or password');
+      return { status: 'UNAUTHORIZED', data: { message: 'Invalid email or password' } };
     }
 
     const isValid = await this.encrypter.compare(password, user.password);
 
     if (!isValid) {
-      throw new Error('Invalid email or password');
+      return { status: 'UNAUTHORIZED', data: { message: 'Invalid email or password' } };
     }
 
     const token = this.tokenGenerator.generate(user);
 
-    return { token };
+    return { status: 'SUCCESSFUL', data: { token } };
   }
 }
