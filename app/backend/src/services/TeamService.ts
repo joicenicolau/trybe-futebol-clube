3e1de72c77c93f98878ed613504c810d4304956e
Joice Nicolau <joice_nicolau@hotmail.com>
2023-06-21 11:59:42 -0300

    alterações na TeamService

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -2,15 +2,13 @@ import TeamModel from '../database/models/TeamModel';
 import { ITeams } from '../Interfaces/team/ITeam';
 
 export default class TeamService {
-  private static _model = TeamModel;
-
   public static async getAllTeams(): Promise<ITeams[]> {
-    const allTeams = await TeamService._model.findAll();
+    const allTeams = await TeamModel.findAll();
     return allTeams;
   }
 
   public static async getTeamById(pk: number): Promise<ITeams> {
-    const team = await TeamService._model.findByPk(pk);
+    const team = await TeamModel.findByPk(pk);
     if (!team) {
       throw new Error('Team not found');
     }
